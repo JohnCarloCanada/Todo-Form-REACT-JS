4883002247e113c88726f89fe6ab6956e2901ba4
John Carlo Cañada <66853423+JohnCarloCanada@users.noreply.github.com>
2023-07-11 20:21:15 +0800

    asd

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,12 +8,19 @@ const TodoList = ({ todos, setFilter }) => {
   const { setTodos } = useContext(DataContext);
   return (
     <>
-      <form className="w-full h-[350px] overflow-y-auto bg-slate-200 p-3 mt-3 rounded-md" onSubmit={(e) => e.preventDefault()}>
+      <form
+        className="w-full h-[350px] overflow-y-auto bg-slate-200 p-3 mt-3 rounded-md"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h2 className="mt-3 text-2xl sm:text-3xl">TODO LIST</h2>
         <label className="text-xl font-bold mr-3" htmlFor="categories">
           Choose a category:
         </label>
-        <select className="font-bold w-44 h-10 border-none outline-none rounded-md " id="categories" onChange={(e) => setFilter(e.target.value)}>
+        <select
+          className="font-bold w-44 h-10 border-none outline-none rounded-md "
+          id="categories"
+          onChange={(e) => setFilter(e.target.value)}
+        >
           <option className="font-semibold" value="all">
             all
           </option>
